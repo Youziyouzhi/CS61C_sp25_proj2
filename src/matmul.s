a86473db2229cddf20872b3bfc84dab1eec09b24
Youzi <grapefruit.libre@gmail.com>
2025-06-12 11:04:16 +0800

    Part B Task6-7 DONE

--- src/matmul.s
+++ src/matmul.s
@@ -1,4 +1,4 @@
-.globl matmul
+.globl matmul 
 
 .text
 # =======================================================
@@ -26,38 +26,96 @@
 matmul:
 
     # Error checks
+    li t0 1
+    blt a1 t0 exception_38
+    blt a2 t0 exception_38
+    blt a4 t0 exception_38
+    blt a5 t0 exception_38
+    blt a2 a4 exception_38
+    
+    # Prologue 
+    addi sp sp -24
+    sw ra 0(sp)
+    sw s0 4(sp)
+    sw s1 8(sp)
+    sw s2 12(sp)
+    sw s3 16(sp)
+    sw s4 20(sp)
+    
+    mv s0 a0 # s0 -> address of the matrix m0   
+    mv s1 a3 # s1 -> address of the matrix m1
+    mv s4 a1 # number of the row of m0
+    mv t2 a2 # number of the array m0 / m1
+    mv t4 a5 # stride of the array m1   
+    li t1 1 # stride of the array m0    
+    li s2 0  # counter m0   
 
+    
+outer_loop_start: # loop matrix A row
 
-    # Prologue
+    li t0 4
+    mul t0 t0 s2
+    mul t0 t0 t2
+    add t0 t0 s0
+    li s3 0 # counter m1
+    
+inner_loop_start: # loop matrix B column , dot and store  
 
+    li t3 4
+    mul t3 t3 s3
+    add t3 t3 s1
+    
+    # Prolugue
+    addi sp sp -28
+    sw t0 0(sp)
+    sw t1 4(sp)
+    sw t2 8(sp)
+    sw t3 12(sp)
+    sw t4 16(sp)
+    sw a5 20(sp)
+    sw a6 24(sp)
+    
+    mv a0 t0
+    mv a1 t3
+    mv a2 t2
+    mv a3 t1
+    mv a4 t4
+    
+    jal ra dot
+    
+    # Epilogue
+    lw t0 0(sp)
+    lw t1 4(sp)
+    lw t2 8(sp)
+    lw t3 12(sp)
+    lw t4 16(sp)    
+    lw a5 20(sp)
+    lw a6 24(sp)
+    addi sp sp 28
+    
+    sw a0 0(a6) # store value
+    addi a6 a6 4
 
-outer_loop_start:
-
-
-
-
-inner_loop_start:
-
-
-
-
-
-
-
-
-
-
-
-
+    addi s3 s3 1
+    bne s3 a5 inner_loop_start
+    
 inner_loop_end:
-
-
-
+    addi s2 s2 1
+    bne s2 s4 outer_loop_start
 
 outer_loop_end:
-
-
+    
     # Epilogue
-
-
+    lw ra 0(sp)
+    lw s0 4(sp)
+    lw s1 8(sp)
+    lw s2 12(sp)
+    lw s3 16(sp)
+    lw s4 20(sp)
+    
+    addi sp sp 24
     jr ra
+    
+exception_38:
+    li a0 38
+    jal x0 exit
--- src/read_matrix.s
+++ src/read_matrix.s
@@ -26,17 +26,133 @@
 # ==============================================================================
 read_matrix:
 
+
+    # step1: fopen + test fopen fail
+    # step2: fread + test fread fail
+    # step3: malloc + test malloc fail
+    # step4：refread + test fread fail 
+    # step4: fclose + test fclose fail
+
     # Prologue
+    addi sp sp -20
+    sw ra 0(sp)
+    sw s0 4(sp)
+    sw s1 8(sp)
+    sw s2 12(sp)
+    sw s3 16(sp)
 
+    # store Arguments
+    mv t0 a0
+    mv s0 a1 # number of rows
+    mv s1 a2 # number of columns
 
+    # fopen
+    mv a0 t0
+    mv a1 x0
 
+    jal fopen
 
+    addi t1 x0 -1
+    beq a0 t1 exception_fopen
+    mv s2 a0 # store the file descriptor 
 
+    # fread rows
+    mv a0 s2
 
+    mv a1 s0
+    addi t0 x0 4
+    mv a2 t0
 
+    addi sp sp -4
+    sw t0 0(sp)
 
+    jal fread
+
+    lw t0 0(sp)
+    addi sp sp 4
+
+    bne a0 t0 exception_fread 
+
+    # fread columns
+    mv a0 s2
+
+    mv a1 s1
+    addi t0 x0 4
+    mv a2 t0
+
+    addi sp sp -4
+    sw t0 0(sp)
+
+    jal fread
+
+    lw t0 0(sp)
+    addi sp sp 4
+
+    bne a0 t0 exception_fread 
+
+    # malloc 
+
+    mul t0 s0 s1 
+    slli t0 t0 2
+    mv a0 t0
+
+    jal malloc
+
+    beq a0 x0 exception_malloc
+    mv s3 a0 # store the first address of the matrix
+
+    # fread matrix
+
+    mv a0 s2
+    mv a1 s3
+
+    lw t0 0(s0)
+    lw t1 0(s1)
+    mul t0 t0 t1
+    slli t0 t0 2
+
+    mv a2 t0
+
+    addi sp sp -4
+    sw t0 0(sp)
+
+    jal fread
+
+    lw t0 0(sp)
+    addi sp sp 4
+
+    bne a0 t0 exception_malloc
+
+    # fclose
+
+    mv a0 s2
+
+    jal fclose
+
+    bne a0 x0 exception_fclose
+
+    mv a0 s3
 
     # Epilogue
 
+    lw ra 0(sp)
+    lw s0 4(sp)
+    lw s1 8(sp)
+    lw s2 12(sp)
+    lw s3 16(sp)
+    addi sp sp 20
 
     jr ra
+
+exception_malloc:
+    li a0 26
+    j exit
+exception_fopen:
+    li a0 27
+    j exit
+exception_fread:
+    li a0 29
+    j exit
+exception_fclose:
+    li a0 28
+    j exit
